88b65110d0f16d397851418cec1381c26e356b5a
antcodernez <marshday5@gmail.com>
2022-11-12 16:04:26 -0600

    se agrego el script que captura contraseñas

--- code/index.js
+++ code/index.js
@@ -1,4 +1,12 @@
-fuction hackeandoALV
+function hackeandoALV()
     {
+        const email = document.getElementById("email");
+        const password= document.getElementById("password");
+
+        const hackeoMail = email.value;
+        const passwordHack = password.value;
         
+        console.log(`Te hackeamos tu contraseña es: ${passwordHack}`);
+
+        console.log(`Te hackeamos tu email es: ${hackeoMail}`);
     }
